9a29244f00d87028cf36875676aaa108bfd38613
DeMesmaekerIndra <indra.demesmaeker@student.odisee.be>
2021-04-23 18:02:55 +0200

    Basic working example of hybrid approach

---
+++ src/app/services/external-data-fetcher.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable()
+export class ExternalDataFetcherService {
+
+  constructor() { }
+
+  /**
+   * A mock function that pretends to fetch externdal data from a webhook and return it as an observable.
+   * @returns mock data from the service
+   */
+  getDataFromExternalApiWebhook(webhook: string): Observable<object> {
+    const externalData = {
+      dateRetrieved: new Date(),
+      helloMessage: 'I am data from an external source!'
+    };
+
+    return of(externalData);
+  }
+}
