f98ac64158b394a799bca0683ea84f844fee66a0
DeMesmaekerIndra <indra.demesmaeker@student.odisee.be>
2021-04-24 15:57:21 +0200

    cleanup

--- src/app/hostComponent/host.module.ts
+++ src/app/hostComponent/host.module.ts
@@ -2,14 +2,9 @@ import { Compiler, CompilerFactory, COMPILER_OPTIONS, NgModule } from '@angular/
 import { CommonModule } from '@angular/common';
 import { HostComponent } from './host.component';
 import { HostRoutingModule } from './host-routing.module';
-import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
-
-export function createCompiler(fn: CompilerFactory): Compiler {
-    return fn.createCompiler();
-}
 
 @NgModule({
-    entryComponents: [
+    declarations: [
         HostComponent
     ],
     imports: [
--- src/app/services/external-data-fetcher.service.ts
+++ src/app/services/external-data-fetcher.service.ts
@@ -12,7 +12,7 @@ export class ExternalDataFetcherService {
    * A mock function that pretends to fetch externdal data from a webhook and return it as an observable.
    * @returns mock data from the service
    */
-  getDataFromExternalApiWebhook(webhook: string): Observable<object> {
+  getDataFromExternalSource(url: string): Observable<object> {
     const externalData = {
       dateRetrieved: new Date(),
       helloMessage: 'I am data from an external source!'
