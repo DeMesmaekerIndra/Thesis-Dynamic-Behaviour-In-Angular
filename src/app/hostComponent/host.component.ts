09bc808b11d41850b05ce5c9c2caa019c571ee68
DeMesmaekerIndra <indra.demesmaeker@student.odisee.be>
2021-05-24 18:39:27 +0200

    cleanup

--- src/app/hostComponent/host.component.ts
+++ src/app/hostComponent/host.component.ts
@@ -5,7 +5,7 @@ import { ExternalComponent } from '../ExternalModules/externalComponent.componen
     templateUrl: './host.component.html'
 })
 export class HostComponent implements AfterViewInit {
-    // HTML element dat als root van de component zal dienen
+    // HTML element dat als parent van de dynamische component zal dienen
     @ViewChild('dynamiccomponent', { read: ViewContainerRef }) container: ViewContainerRef;
 
     constructor(private injector: Injector, private compiler: Compiler) { }
@@ -15,12 +15,12 @@ export class HostComponent implements AfterViewInit {
         import('../ExternalModules/external.module')
             // Terugkrijgen van een referentie naar de module klasse
             .then(moduleClassRef =>
-                // Het compileren van de module klasse naar zijn factories
+                // Het compileren van de module klasse naar zijn factory
                 this.compiler.compileModuleAndAllComponentsAsync(moduleClassRef.ExternalModule).then((compiledModule => {
-                    // Module instantie maken met factory, injector meegeven om dependency injection toe te laten
+                    // Module instantie maken met factory, dependency Injector meegeven
                     const module = compiledModule.ngModuleFactory.create(this.injector);
 
-                    // Zoeken naar gewenste component factory met de componentFactoryResolver
+                    // Resolver gebruiken om een component factory te vinden op basis van type
                     const componentFactory = module.componentFactoryResolver.resolveComponentFactory(ExternalComponent);
 
                     // Instantie van component maken en vasthangen aan parent element
